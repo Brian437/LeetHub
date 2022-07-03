14dc17b33b893de3deb82ea132eab40f32aa98e7
Brian437 <brianchaves7@gmail.com>
2022-07-03 16:54:44 -0400

    Time: 166 ms (7.07%), Space: 38.1 MB (25.05%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public string IntToRoman(int num) {
+        Dictionary<string,int> romanNumerals=new Dictionary<string,int>();
+        
+        romanNumerals.Add("M",1000);
+        romanNumerals.Add("CM",900);
+        romanNumerals.Add("D",500);
+        romanNumerals.Add("CD",400);
+        romanNumerals.Add("C",100);
+        romanNumerals.Add("XC",90);
+        romanNumerals.Add("L",50);
+        romanNumerals.Add("XL",40);
+        romanNumerals.Add("X",10);
+        romanNumerals.Add("IX",9);
+        romanNumerals.Add("V",5);
+        romanNumerals.Add("IV",4);
+        romanNumerals.Add("I",1);
+        
+        string roman="";
+        int index=0;
+        while(num>0)
+        {
+            int number=romanNumerals.ElementAt(index).Value;
+            string letter=romanNumerals.ElementAt(index).Key;
+            
+            // Console.WriteLine("========");
+            // Console.WriteLine("Number:"+number);
+            // Console.WriteLine("Letter:"+letter);
+            // Console.WriteLine("num:"+num);
+            
+            if(romanNumerals.ElementAt(index).Value<=num)
+            {
+                roman+=letter;
+                num-=number;
+            }
+            else
+            {
+                index++;
+            }
+        }
+        
+        Console.WriteLine(romanNumerals.ElementAt(0).Key);
+        Console.WriteLine(romanNumerals.ElementAt(0).Value);
+        return roman;
+    }
+}
