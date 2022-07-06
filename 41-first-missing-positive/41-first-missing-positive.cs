3bafbb01d3c46ec047db62d13fa1f7eab8cccb78
Brian437 <brianchaves7@gmail.com>
2022-07-06 19:44:23 -0400

    Time: 994 ms (5.11%), Space: 64.7 MB (44.48%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cs
+++ 41-first-missing-positive/41-first-missing-positive.cs
@@ -1,14 +1,9 @@
 public class Solution {
     public int FirstMissingPositive(int[] nums) {
-        // List<int> numList=new List<int>();
         bool[] numInList=new bool[nums.Length];
         
         for(int i=0;i<nums.Length;i++)
         {
-            // if(!numList.Contains(nums[i]) && nums[i]>0)
-            // {
-                // numList.Add(nums[i]);
-            // }
             if(nums[i]<=nums.Length && nums[i]>0)
             {
                 Console.WriteLine(nums[i]+"/"+i);
@@ -24,19 +19,6 @@ public class Solution {
             }
         }
         
-        // int num=1;
-        
-        // while(true)
-        // {
-        //     if(numList.Contains(num))
-        //     {
-        //         num++;
-        //     }
-        //     else
-        //     {
-        //         return num;
-        //     }
-        // }
         return nums.Length+1;
     }
 }
